38608ec95ba5970d00bf94381e1dd630b55dbea7
SpringPie <bente.vandepol@gmail.com>
2021-03-23 17:30:10 +0100

    Template was slightly different than test, adjusted code DAT-6078 [skip ci]

--- functions/consume-email/emailprocessor/emailprocessor.py
+++ functions/consume-email/emailprocessor/emailprocessor.py
@@ -1,15 +1,15 @@
-from config import SENDERS, ID, REQUIRED_FIELDS, TOPIC_NAME, TOPIC_PROJECT_ID
-import logging
 import json
+import logging
+
+from bs4 import BeautifulSoup
+from config import ID, REQUIRED_FIELDS, SENDERS, TOPIC_NAME, TOPIC_PROJECT_ID
 from gobits import Gobits
 from google.cloud import pubsub_v1
-from bs4 import BeautifulSoup
 
 logging.basicConfig(level=logging.INFO)
 
 
 class EmailProcessor(object):
-
     def __init__(self):
         self.senders = SENDERS
         self.required_fields = REQUIRED_FIELDS
@@ -31,20 +31,26 @@ class EmailProcessor(object):
             date = mail["Datum"]
         if mail_sender not in self.senders:
             if date:
-                logging.info("Mail received on {} was not send by the right e-mail address".format(date))
+                logging.info(
+                    "Mail received on {} was not send by the right e-mail address".format(
+                        date
+                    )
+                )
                 return False
             else:
                 logging.info("Mail received was not send by the right e-mail address")
                 return False
         html_content = mail["body"]
         # Check if code contains "<table>", "<tr>" and "<td>" tags
-        if "<table>" not in html_content or \
-           "<tr>" not in html_content or \
-           "<td>" not in html_content:
+        if (
+            "<table>" not in html_content
+            or "<tr>" not in html_content
+            or "<td>" not in html_content
+        ):
             logging.info("Required tags cannot be found in HTML body")
             return False
         # Get part before table
-        html_above_table = html_content.split('<table>')[0]
+        html_above_table = html_content.split("<table>")[0]
         # Check if the part is not empty
         if not html_above_table:
             logging.info("HTML body does not have the required structure")
@@ -66,6 +72,9 @@ class EmailProcessor(object):
             value = ""
             # If the line contains information
             if line:
+                # Remove '&nbsp;' from line
+                if "&nbsp;" in line:
+                    line = line.replace("&nbsp;", " ")
                 # Split the line on ': <<'
                 line_list = line.split(": &lt;&lt;")
                 # The first value of the line is the field
@@ -75,32 +84,31 @@ class EmailProcessor(object):
                     # If there were also ':' in the value of the field,
                     # there are multiple list items after the first
                     # Combine them again
-                    value = ''.join(line_list)
+                    value = "".join(line_list)
             if value:
                 # Remove '<<' and '>>'
-                value = value.replace('&lt;&lt;', '')
-                value = value.replace('&gt;&gt;', '')
+                value = value.replace("&lt;&lt;", "")
+                value = value.replace("&gt;&gt;", "")
                 # Check if value still has a value
                 if value:
                     #  If the first character is a whitespace, remove it
                     if value[0] == " ":
-                        value = value.replace(' ', '', 1)
+                        value = value.replace(" ", "", 1)
             if field:
                 new_message = self.add_field(field, value, new_message)
         # HTML to parse-able content
-        parsed_html = BeautifulSoup(html_content, 'html.parser')
+        parsed_html = BeautifulSoup(html_content, "html.parser")
         # Get table part
         table = parsed_html.table
         # Get values from the table
-        lines = table.find_all('tr')
+        lines = table.find_all("tr")
         for line in lines:
             # Split on tag 'td'
-            td_list = line.find_all('td')
+            td_list = line.find_all("td")
             # Field
             field = td_list[0].get_text()
-            # Remove ':' from end of field
-            if field.endswith(":"):
-                field = field[:-1]
+            # Split field on ':' and get first value
+            field = field.split(":")[0]
             # value
             value = td_list[1].get_text()
             if field:
@@ -108,12 +116,10 @@ class EmailProcessor(object):
         # Check if every required field was added
         added_field_count = 0
         for field in self.required_fields:
-            field = field.replace(' ', '_')
+            field = field.replace(" ", "_")
             field = field.lower()
             if field not in new_message:
-                dict_line = {
-                    field: ""
-                }
+                dict_line = {field: ""}
                 new_message.update(dict_line)
                 added_field_count = added_field_count + 1
         # Check if added fields are not equal to all the fields
@@ -123,7 +129,7 @@ class EmailProcessor(object):
         # Add an ID
         received_on_list = mail["received_on"].split("+")
         received_on = received_on_list[0]
-        received_on = received_on.replace(':', '-')
+        received_on = received_on.replace(":", "-")
         subject_mail = mail["subject"]
         ticket_nr = ""
         for ch in range(len(subject_mail)):
@@ -131,7 +137,7 @@ class EmailProcessor(object):
                 while True:
                     if ch + 1 > len(subject_mail):
                         break
-                    if subject_mail[ch+1] == "]":
+                    if subject_mail[ch + 1] == "]":
                         break
                     ch = ch + 1
                     ticket_nr = ticket_nr + subject_mail[ch]
@@ -141,7 +147,13 @@ class EmailProcessor(object):
         if not new_message[self.parsed_email_id]:
             logging.error(f"ID {self.parsed_email_id} is empty in message")
             return False
-        new_message.update({"id": "{}_{}_{}".format(new_message[self.parsed_email_id], ticket_nr, received_on)})
+        new_message.update(
+            {
+                "id": "{}_{}_{}".format(
+                    new_message[self.parsed_email_id], ticket_nr, received_on
+                )
+            }
+        )
         metadata = Gobits()
         return_bool_publish_topic = self.publish_to_topic(new_message, metadata)
         if not return_bool_publish_topic:
@@ -150,8 +162,8 @@ class EmailProcessor(object):
 
     def add_field(self, field, value, message):
         if field in self.required_fields:
-            field = field.replace(' ', '_')
-            field = field.replace(':', '')
+            field = field.replace(" ", "_")
+            field = field.replace(":", "")
             field = field.lower()
             # Check if field can already be found in message
             if field in message:
@@ -164,15 +176,13 @@ class EmailProcessor(object):
                     count = count + 1
             # Remove HTML whitespace code from value
             if "&amp;nbsp;" in value:
-                value = value.replace('&amp;nbsp;', ' ')
+                value = value.replace("&amp;nbsp;", " ")
             if "&nbsp;" in value:
-                value = value.replace('&nbsp;', ' ')
+                value = value.replace("&nbsp;", " ")
             # Remove non breaking whitespace HTML code from value
-            if u"\u00a0" in value:
-                value = value.replace(u'\u00a0', ' ')
-            dict_line = {
-                field: value
-            }
+            if "\u00a0" in value:
+                value = value.replace("\u00a0", " ")
+            dict_line = {field: value}
             message.update(dict_line)
         return message
 
@@ -180,26 +190,26 @@ class EmailProcessor(object):
         date = ""
         if "Datum" in message:
             date = message["Datum"]
-        msg = {
-            "gobits": [gobits.to_json()],
-            "parsed_email": message
-        }
+        msg = {"gobits": [gobits.to_json()], "parsed_email": message}
         try:
             # Publish to topic
             publisher = pubsub_v1.PublisherClient()
             topic_path = "projects/{}/topics/{}".format(
-                self.topic_project_id, self.topic_name)
+                self.topic_project_id, self.topic_name
+            )
             future = publisher.publish(
-                topic_path, bytes(json.dumps(msg).encode('utf-8')))
+                topic_path, bytes(json.dumps(msg).encode("utf-8"))
+            )
             if date:
                 future.add_done_callback(
-                    lambda x: logging.debug('Published parsed email with date {}'.format(date))
+                    lambda x: logging.debug(
+                        "Published parsed email with date {}".format(date)
+                    )
                 )
-            future.add_done_callback(
-                lambda x: logging.debug('Published parsed email')
-            )
+            future.add_done_callback(lambda x: logging.debug("Published parsed email"))
             return True
         except Exception as e:
-            logging.exception('Unable to publish parsed email ' +
-                              'to topic because of {}'.format(e))
+            logging.exception(
+                "Unable to publish parsed email " + "to topic because of {}".format(e)
+            )
         return False
